c42eb3768cb133a886a3820786c8543c6cf2bd2f
CokeAddict <tomboehmdd@gmail.com>
2016-06-07 20:21:31 +0000

    Add files via upload
    to do:
     - Ausgabe einrichten
     - Inputvarianten schreiben

--- HexDumpPanel.java
+++ HexDumpPanel.java
@@ -18,51 +18,99 @@ import javafx.stage.Stage;
 /**
  * Created by Tom on 07.06.2016.
  */
-public class HexDumpPanel extends Application{
-    public static void main(String[] args){
-        launch();
+public class HexDumpPanel extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) {
+
+        Parameters parameters = getParameters();
+
         primaryStage.setTitle("HexDump");
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
-        grid.setVgap(10);
+        grid.setVgap(15);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
         //Content of the panel
         Text sceneTitle = new Text("What type of input?");
-            sceneTitle.setFont(Font.font("Arial", FontWeight.NORMAL, 20));
-            grid.add(sceneTitle, 0, 0, 2, 1);
+        sceneTitle.setFont(Font.font("Arial", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
 
         Label inputType = new Label("Type:");
-            grid.add(inputType, 0, 1);
+        grid.add(inputType, 0, 1);
 
         TextField inputTextField = new TextField();
-            grid.add(inputTextField, 1, 1);
+        grid.add(inputTextField, 1, 1);
 
-        Button button = new Button("generate HexDump");
-            HBox hbButton = new HBox(10);
-            hbButton.setAlignment(Pos.BASELINE_LEFT);
-            hbButton.getChildren().add(button);
-            grid.add(hbButton, 1, 3);
+        Button button = new Button("generate Hexdump");
+        HBox hbButton = new HBox(0);
+        hbButton.setAlignment(Pos.BASELINE_CENTER);
+        hbButton.getChildren().add(button);
+        grid.add(hbButton, 0, 2, 2, 1);
 
         final Text actionTarget = new Text();
-            grid.add(actionTarget, 1, 5);
+        grid.add(actionTarget, 0, 3, 2, 1);
 
         //Events
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 actionTarget.setFill(Color.CRIMSON);
-                actionTarget.setText("work in progress");
+                actionTarget.setText("something");
+
             }
         });
 
-        Scene scene = new Scene(grid, 300, 200);
+        Scene scene = new Scene(grid, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    byte data[];
+
+    private char[] hexByte(int z, int len) {
+        char[] x = new char[len];
+        int i, hx;
+        for (i = len - 1; i >= 0; i--) {
+            hx = z;
+            z >>>= 4;
+            hx &= 0xf;
+            x[i] = (char) (hx <= 9 ? hx + '0' : hx + 'A' - 10);
+        }
+        return x;
+    }
+
+    public void getHexString() {
+        int i, j, line = 0;
+        String s = new String(hexByte(line, 4)) + ": ";
+        String temp;
+        String plainText;
+
+        String dataString = new String(data);
+
+        for (i = 0; i < dataString.length(); i++) {
+            temp = new String(hexByte(data[i], 2));
+            s = s + temp;
+
+            if ((i + 1) % 16 == 0 && (i + 1) != dataString.length()) {
+                line++;
+                temp = new String(hexByte(line, 4));
+                plainText = "";
+
+                for (j = (i - 15); j <= i; j++) {
+                    plainText = plainText + new String(hexByte(data[j], 1));
+                }
+
+                s = s + "\t\t" + plainText + "\n" + temp + ": ";
+            } else if ((i + 1) % 4 == 0 && (i + 1) != dataString.length()) {
+                s = s + " | ";
+            } else {
+                s = s + " ";
+            }
+        }
     }
 }
