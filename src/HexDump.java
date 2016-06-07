75b76f933e7ac3754ef7fca551ecc4c57bb5d558
Edwin Scharfe <scharfe.edwin@gmail.com>
2016-06-07 20:34:12 +0200

    Toms Stand übernommen

--- src/HexDump.java
+++ src/HexDump.java
@@ -1,38 +1,27 @@
 import java.lang.*;
 import java.io.*;
-import java.util.Arrays;
-
 /**
  * Created by Edwin on 26/05/2016.
  */
 public class HexDump
 {
 
-    private byte data[];
+    byte data[];
 
-    public static void main(String args[]){
-
-        try
-        {
-            System.out.println(new HexDump(args[0]).getHexString());
-            //System.out.println(new HexDump(new FileInputStream(args[0])).getHexString());
-        }catch(Exception e){System.out.println(e);}
-    }
-
-    private HexDump(String s){
+    HexDump(String s)
+    {
         data=s.getBytes();
     }
 
-    private HexDump(InputStream fis){
+    HexDump(InputStream fis)
+    {
         try
         {
             ByteArrayOutputStream bos=new ByteArrayOutputStream(1024);
             byte buf[]=new byte[1024];
             int lenr;
 
-            while ((lenr=fis.read(buf))>-1){
-                bos.write(buf,0,lenr);
-            }
+            while ((lenr=fis.read(buf))>-1) bos.write(buf,0,lenr);
 
             data=bos.toByteArray();
         }catch(Exception e){System.out.println(e);}
@@ -40,7 +29,8 @@ public class HexDump
 
     /* produces a char Array of length len displaying
        the hexadecimal Value of the value z */
-    private char[] hexByte(int z, int len){
+    private char[] hexByte(int z, int len)
+    {
         char[] x=new char[len];
         int i,hx;
         for(i=len-1;i>=0;i--)
@@ -53,49 +43,49 @@ public class HexDump
         return x;
     }
 
-    private String HexByteString(int pos){
-        return Arrays.toString(hexByte(data[pos], 2));
-    }
-
-    /* produces a String in the Form of a HexDump
+    /* produces a String in the Form of a hexdump
        of an array of bytes */
-    private String getHexString(){
-        String s = "";
-        int line = 0;
+    public String getHexString()
+    {
+        int i, j, line = 0;
+        String s = new String(hexByte(line, 4)) + ": ";
+        String temp;
+        String plainText;
 
-        String dataString = Arrays.toString(data);
+        String dataString = new String(data);
 
-        s = s + "" + Arrays.toString(hexByte(line, 4));
+        for (i = 0; i < dataString.length(); i++) {
+            temp = new String(hexByte(data[i], 2));
+            s = s + temp;
 
-        for (int i = 0; i < dataString.length(); i++) {
-            s = s + HexByteString(i);
+            if((i+1) % 16 == 0 && (i+1) != dataString.length()) {
+                line++;
+                temp = new String(hexByte(line, 4));
+                plainText = "";
 
-            if (i % 4 == 0) {
-                s = s + " | ";
-            } else {
-                s = s + " ";
+                for (j = (i-15); j <= i; j++) {
+                    plainText = plainText + new String(hexByte(data[j], 1));
+                }
+
+                s = s + "\t\t" + plainText + "\n" + temp + ": ";
             }
-
-            if (i % 16 == 0){
-                line ++;
-                s = s + "\n" + Arrays.toString(hexByte(line, 4));
+            else if((i+1) % 4 == 0 && (i+1) != dataString.length()){
+                s = s + " | ";
+            }
+            else{
+                s = s + " ";
             }
         }
 
-
-
-
-
-        // geben Sie zuerst den Text in data als String zurueck, erzeugen Sie dazu ein
-        // neues Objekt der Klasse String und initialisieren es mit data.
-        // Als zweites erzeugen Sie ein Stringobjekt aus dem Returnwert der
-        // Funktion getHexByte, der Sie das erste Byte von Data und die
-        // Laenge 2 uebergeben.
-        // Als drittes bauen Sie eine Schleife, in der die Bytes aus Data
-        // mit getHexByte umgewandelt werden und nacheinander, durch Leerzeichen
-        // getrennt zu dem String zusammen gebaut werden.
-
         return s;
     }
+    public static void main(String args[])
+    {
 
+        try
+        {
+            System.out.println(new HexDump(args[0]).getHexString());
+            //System.out.println(new HexDump(new FileInputStream(args[0])).getHexString());
+        }catch(Exception e){System.out.println(e);}
+    }
 }
---
+++ src/HexDumpPanel.java
@@ -0,0 +1,69 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ * Created by Edwin on 07/06/2016.
+ */
+
+public class HexDumpPanel extends Application{
+    public static void main(String[] args){
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("HexDump");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        //Content of the panel
+        Text sceneTitle = new Text("What type of input?");
+        sceneTitle.setFont(Font.font("Arial", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+
+        Label inputType = new Label("Type:");
+        grid.add(inputType, 0, 1);
+
+        TextField inputTextField = new TextField();
+        grid.add(inputTextField, 1, 1);
+
+        Button button = new Button("generate HexDump");
+        HBox hbButton = new HBox(10);
+        hbButton.setAlignment(Pos.BASELINE_LEFT);
+        hbButton.getChildren().add(button);
+        grid.add(hbButton, 1, 3);
+
+        final Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 5);
+
+        //Events
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                actionTarget.setFill(Color.CRIMSON);
+                actionTarget.setText("work in progress");
+            }
+        });
+
+        Scene scene = new Scene(grid, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
